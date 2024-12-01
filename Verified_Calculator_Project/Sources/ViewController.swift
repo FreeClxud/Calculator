46d66157db1d9678f776a27a4ca0736acccec41c
FreeClxud <Romerom6470@durhamtech.edu>
2024-12-01 12:32:24 -0500

    Add files via upload

---
+++ Verified_Calculator_Project/Sources/ViewController.swift
@@ -0,0 +1,10 @@
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Additional setup for the calculator app
+    }
+}
